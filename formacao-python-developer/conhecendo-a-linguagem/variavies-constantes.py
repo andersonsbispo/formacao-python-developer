4722ccdb4fb1dbbbe4fb86c51b3571221a0352d2
Anderson Bispo <andersonsbispo1997@outlook.com>
2024-02-04 02:10:01 -0300

    Fim modulo: Conhecendo a Linguagem de Programação Python!

---
+++ formacao-python-developer/conhecendo-a-linguagem/variavies-constantes.py
@@ -0,0 +1,14 @@
+# VARIAVEIS
+nome = "Anderson";
+idade = "26";
+
+nome, idade = ("Bruce", 45);
+
+print(nome, idade);
+
+limite_saque_diario = "1000";
+
+#CONSTANTES (No Python não existem constantes, então como convenção as constatantes são escritas em letras maiusculas)
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"];
+
+print(BRAZILIAN_STATES);
